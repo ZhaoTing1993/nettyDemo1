b8c18701c64f6e1288a8a6a80fd59b290fddc084
Ting <zt_26@qq.com>
2018-03-21 21:57:02 +0800

    channel & selector

--- socketdemo1/src/main/java/Client.java
+++ socketdemo1/src/main/java/Client.java
@@ -1,53 +1,40 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
 public class Client {
-    private static int PORT = 8379;
-    private static String IP = "127.0.0.1";
-
     public static void main(String[] args) {
-        BufferedReader bufferedReader = null;
-        PrintWriter printWriter = null;
-        Socket socket = null;
-        try {
-            socket = new Socket(IP, PORT);
-            //autoFlush: if true, the println,printf or format methods will flush the output buffer
-            //使用printWriter将信息写到输出流中
-            printWriter = new PrintWriter(socket.getOutputStream(), true);
-            bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        InetSocketAddress address = new InetSocketAddress("127.0.0.1", 8379);
+        SocketChannel sc = null;
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
 
-            //由于autoFlush为true，printWriter使用println会在写入outputStream后立即flush，服务端会马上从inputStream中读到消息
-            printWriter.println("客户端请求了服务器....");
-            String response = bufferedReader.readLine();
-            System.out.println("Client：" + response);
+        try {
+            //打开通道
+            sc = SocketChannel.open();
+            //建立连接
+            sc.connect(address);
+            while (true) {
+                byte[] bytes = new byte[1024];
+                System.in.read(bytes);
+                //把输入的数据放入buffer缓冲区
+                buffer.put(bytes);
+                //复位操作
+                buffer.flip();
+                //将buffer的数据写入通道
+                sc.write(buffer);
+                //清空缓冲区中的数据
+                buffer.clear();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            if (bufferedReader != null) {
+            if (sc != null) {
                 try {
-                    bufferedReader.close();
+                    sc.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-            }
-            if (printWriter != null) {
-                try {
-                    printWriter.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            if (socket != null) {
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                socket = null;
             }
         }
     }
