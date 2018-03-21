9164519050f6c8b2c72d6e81e8c4289e652ec55f
Ting <zt_26@qq.com>
2018-03-21 23:54:16 +0800

    asyncSocketChannel

--- socketdemo1/src/main/java/Server.java
+++ socketdemo1/src/main/java/Server.java
@@ -1,32 +1,39 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.channels.AsynchronousChannelGroup;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+/**
+ * Created by Ting on 2018/3/21.
+ */
 public class Server {
-    private static int PORT = 8379;
+    //线程池
+    private ExecutorService executorService;
+    //线程组
+    private AsynchronousChannelGroup channelGroup;
+    //服务器通道
+    private AsynchronousServerSocketChannel channel;
 
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
+    public Server(int port) {
         try {
-            serverSocket = new ServerSocket(PORT);
-            System.out.println("服务器端启动了....");
-            while (true) {
-                //进行阻塞
-                Socket socket = serverSocket.accept();
-                //启动一个线程来处理客户端请求
-                new Thread(new ServerHandler(socket)).start();
-            }
+            //创建线程池
+            executorService  = Executors.newCachedThreadPool();
+            //创建线程组
+            channelGroup = AsynchronousChannelGroup.withCachedThreadPool(executorService, 1);
+            //创建服务器通道
+            channel = AsynchronousServerSocketChannel.open(channelGroup);
+            //绑定地址
+            channel.bind(new InetSocketAddress(port));
+            System.out.println("server start, port：" + port);
+            channel.accept(this, new ServerCompletionHandler());
+            Thread.sleep(Integer.MAX_VALUE);
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (serverSocket != null) {
-                try {
-                    serverSocket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            serverSocket = null;
         }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server(8379);
     }
 }
